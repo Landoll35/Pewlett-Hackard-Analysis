bf6d2cf53e1b1e7dd1c812982192a24d1767d1f7
Landoll35 <97926019+Landoll35@users.noreply.github.com>
2022-12-07 21:55:57 -0700

    Add files via upload

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,47 @@
+-- Create retirement_titles table 
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles
+FROM employees as e
+LEFT JOIN titles as ti
+ON e.emp_no = ti.emp_no
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Distinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles as rt
+WHERE rt.to_date = ('9999-01-01')
+ORDER BY emp_no, title DESC;
+
+SELECT COUNT (ut.emp_no),
+	ut.title
+INTO retiring_titles
+FROM unique_titles as ut
+GROUP BY title
+ORDER BY COUNT (title) DESC;
+
+-- Mentorship eligibilty query
+SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+    e.first_name, 
+    e.last_name, 
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+    ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+Left outer Join dept_emp as de
+ON (e.emp_no = de.emp_no)
+Left outer Join titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
